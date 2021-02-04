ce94913c9c6646fdd393c3d9f194884ddc35a618
Mahendra <mahe7461@gmail.com>
2021-02-04 17:14:56 +0530

    task

---
+++ Entering data in table TASK.sql
@@ -0,0 +1,41 @@
+select * from CHUNKS
+select * from DEPENDENCY_JOBS
+select * from JOBS
+select * from SOURCES
+insert into SOURCES
+values (1,'Raman','Ram','R',11,'none','none',2,10,'local','raman@1','asedfrgy',1,'file:cwddf/fdfr','ring',2,'c drive','complete the level1 project','live',12456,'IST','indian time', 'ram@gmail.com','ten','2020-11-04','2021-01-05')
+insert into SOURCES
+values (2,'Rajan','Raj','R',11,'none','day',3,11,'local','rajan@1','asedfrkio',2,'file:cwddf/fdfr','ring',4,'c drive','complete the level1 project','live',12478,'IST','indian time', 'rajan@gmail.com','two','2020-11-03','2021-01-08'),
+(3,'Mani','mani','P',11,'none','none',2,10,'local','mani@1','asedpokgy',1,'file:cwddf/fdfr','ring',2,'c drive','complete the level1 project','live',12456,'IST','indian time', 'ram@gmail.com','ten','2020-11-04','2021-01-05'),
+(4,'Karan','Karan','R',11,'none','none',2,10,'local','karan@1','asiuiofrgy',1,'file:cwddf/fdfr','ring',2,'c drive','complete the level2 project','live',124976,'IST','indian time', 'karan@gmail.com','ten','2020-11-09','2021-01-06'),
+(5,'Samsun','Sam','H',11,'none','none',2,10,'local','sam@1','asedfiuy',1,'file:cwddf/fdfr','ring',2,'c drive','complete the level2 project','live',124897,'IST','indian time', 'sam@gmail.com','ten','2020-11-08','2021-01-08'),
+(6,'Kumar','Kumar','T',11,'none','none',2,10,'local','kumar@1','asopdfrgy',1,'file:cwddf/fdfr','ring',2,'c drive','complete the level2 project','live',12986,'IST','indian time', 'kumar@gmail.com','ten','2020-11-12','2021-01-05'),
+(7,'vignesh','vicky','R',11,'none','none',2,10,'local','vicky@1','asedflky',1,'file:cwddf/fdfr','ring',2,'c drive','complete the level1 project','live',129896,'IST','indian time', 'vicky@gmail.com','ten','2020-11-04','2021-01-05'),
+(8,'Ramesh','Ramesh','s',91,'none','none',2,10,'local','ramesh@1','asghlfrgy',1,'file:cwddf/fdfr','ring',2,'c drive','complete the level1 project','live',12466,'IST','indian time', 'ramesh45@gmail.com','ten','2020-11-12','2021-01-05'),
+(9,'Rajan','Rajan','R',11,'none','none',2,10,'local','rajan@45','auldfrgy',1,'file:cwddf/fdfr','ring',2,'c drive','complete the level1 project','live',12096,'IST','indian time', 'rajan67@gmail.com','ten','2020-11-04','2021-01-08'),
+(10,'Guna','Guna','L',11,'none','none',2,10,'local','guna@1','asehyurgy',1,'file:cwddf/fdfr','ring',2,'c drive','complete the level1 project','live',178456,'IST','indian time', 'guna@gmail.com','ten','2020-11-04','2021-01-10')
+insert into JOBS
+VALUES (1,'John',1,'no commands','On precess',2,'2020-11-24','2021-01-02','2021-01-12','2021-02-02','Its is pid','c drive','john8@gmail.com','ten','2020-10-23','2021-01-06'),
+(2,'Siva',5,'no commands','On precess',2,'2020-12-24','2021-01-08','2021-01-17','2021-01-29','Its is pid','c drive','siva78@gmail.com','ten','2020-10-20','2021-01-09'),
+(3,'Kumaran',3,'no commands','Finished',2,'2020-12-20','2021-01-16','2021-01-20','2021-02-01','Its is pid','c drive','kumaran85@gmail.com','two','2020-10-26','2021-01-07'),
+(4,'Ganesh',1,'no commands','On precess',2,'2020-11-24','2021-01-06','2021-01-19','2021-02-02','Its is pid','c drive','ganesh8@gmail.com','three','2020-10-24','2021-01-06'),
+(5,'Nila',1,'no commands','On precess',2,'2020-11-24','2021-01-08','2021-01-17','2021-02-01','Its is pid','c drive','nila8@gmail.com','four','2020-10-28','2021-01-19'),
+(6,'chandra',5,'no commands','On precess',2,'2020-11-24','2021-01-02','2021-01-10','2021-02-02','Its is pid','c drive','chandra65@gmail.com','ten','2020-10-23','2021-01-06'),
+(7,'Nivas',2,'no commands','On precess',2,'2020-11-24','2021-01-02','2021-01-12','2021-02-02','Its is pid','c drive','nivas8@gmail.com','ten','2020-10-23','2021-01-06'),
+(8,'samsun',9,'no commands','On precess',2,'2020-11-24','2021-01-02','2021-01-12','2021-02-02','Its is pid','c drive','san8@gmail.com','ten','2020-10-23','2021-01-06'),
+(9,'sudhakaran',6,'no commands','On precess',2,'2020-11-24','2021-01-02','2021-01-12','2021-02-02','Its is pid','c drive','ramn8@gmail.com','ten','2020-10-23','2021-01-06'),
+(10,'vijay',3,'no commands','On precess',2,'2020-11-24','2021-01-02','2021-01-02','2021-02-03','Its is pid','c drive','karn8@gmail.com','ten','2020-10-23','2021-01-06');
+insert into CHUNKS
+values (1,5,'Tmp-Path','c: drive','2020-10-12','2021-01-09'),
+(2,4,'Tmp-Path','c: drive','2020-10-10','2021-01-19'),
+(3,5,'Tmp-Path','c: drive','2020-10-12','2021-01-08'),
+(4,8,'tmp-Path','c: drive','2020-10-10','2021-01-07'),
+(5,4,'Tmp-Path','c: drive','2020-10-18','2021-01-08'),
+(6,1,'Tmp-Path','c: drive','2020-10-19','2021-01-02'),
+(7,9,'Tmp-Path','c: drive','2020-10-14','2021-01-04'),
+(8,6,'Tmp-Path','c: drive','2020-10-12','2021-01-06'),
+(9,4,'Tmp-Path','c: drive','2020-10-23','2021-01-09'),
+(10,5,'Tmp-Path','c: drive','2020-10-26','2021-01-07');
+insert into DEPENDENCY_JOBS
+values(1,2,3),
+(2,2,3),(3,4,3),(4,8,2),(5,2,3),(6,4,1),(7,8,7),(8,8,5),(9,3,9),(10,4,6);
